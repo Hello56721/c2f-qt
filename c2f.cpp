9ce87d892cdcc58bfa248ca14cf721d590607e2a
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-06-08 10:05:20 -0400

    Added the code to convert temperature

--- c2f.cpp
+++ c2f.cpp
@@ -1,6 +1,46 @@
 #include "c2f.h"
 #include "./ui_c2f.h"
 
+#include <string>
+
+void C2F::f2c() {
+    QString cqstr = ui->f2cInput->text();
+    QString result;
+    
+    bool success;
+    double f = cqstr.toDouble(&success);
+    if (!success) {
+        result = "Result: NaN";
+        ui->f2cResultLabel->setText(result);
+        return;
+    }
+    
+    double c = (f - 32) * 5 / 9;
+    
+    std::string resultStr = std::to_string(c);
+    result = QString("<b style=\"font-size: 11pt\">Result: ") + resultStr.c_str() + "</b>";
+    ui->f2cResultLabel->setText(result);
+}
+
+void C2F::c2f() {
+    QString cqstr = ui->c2fInput->text();
+    QString result;
+    
+    bool success;
+    double c = cqstr.toDouble(&success);
+    if (!success) {
+        result = "Result: NaN";
+        ui->c2fResultLabel->setText(result);
+        return;
+    }
+    
+    double f = (c * 9 / 5) + 32;
+    
+    std::string resultStr = std::to_string(f);
+    result = QString("<b style=\"font-size: 11pt\">Result: ") + resultStr.c_str() + "</b>";
+    ui->c2fResultLabel->setText(result);
+}
+
 C2F::C2F(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::C2F)
@@ -11,5 +51,29 @@ C2F::C2F(QWidget *parent)
 C2F::~C2F()
 {
     delete ui;
+}
+
+
+void C2F::on_f2cInput_returnPressed()
+{
+    f2c();
+}
+
+
+void C2F::on_c2fInput_returnPressed()
+{
+    c2f();
+}
+
+
+void C2F::on_c2fConvertButton_clicked()
+{
+    c2f();
+}
+
+
+void C2F::on_f2cConvertButton_clicked()
+{
+    f2c();
 }
 
--- c2f.h
+++ c2f.h
@@ -11,9 +11,24 @@ class C2F : public QMainWindow
 {
     Q_OBJECT
     
+    // Process the Fahrenheit to Celsius
+    void f2c();
+    
+    // Process the Celsius to Frahrenheit
+    void c2f();
+    
 public:
     C2F(QWidget *parent = nullptr);
     ~C2F();
+    
+private slots:
+    void on_f2cInput_returnPressed();
+    
+    void on_c2fInput_returnPressed();
+    
+    void on_c2fConvertButton_clicked();
+    
+    void on_f2cConvertButton_clicked();
     
 private:
     Ui::C2F *ui;
